536249c9ce00086552cb723bb33fd5a1eaad372d
William Mitchell <billy.mitchell@levvel.com>
2018-08-14 13:01:34 -0400

    Added typing component to entry page

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -4,6 +4,8 @@ import * as routes from "../../constants/routes";
 import React, { Component } from "react";
 import "./Entry.css";
 import video from "./video.mp4";
+import Typing from 'react-typing-animation';  
+
 
 
 class Entry extends Component {
@@ -12,7 +14,12 @@ class Entry extends Component {
   //     this.state = {...}
   // }
 
+  
+  
+
   render() {
+  
+    
     return (
      
      <div className = "entry-body">   
@@ -28,8 +35,43 @@ class Entry extends Component {
         </div>
 
         <div className = "content"> 
-          <h1>  Video background tutorial   </h1> 
-          <p> this tutorial shows you how to put a video as a background for your website </p> 
+          <h1>  Local EDM, A place to:</h1> 
+          <p> <Typing speed = {70} loop = {true} delay ={500}> 
+            
+            
+            
+            <span>
+             Listen to Artists in Your Area. 
+            </span>
+
+            <Typing.Backspace count = {31} delay = {500} />            
+            <Typing.Delay ms = {0} />
+            
+            <span>
+             Upload Your Own Music. 
+            </span>   
+            <Typing.Backspace count = {22} delay = {500} />          
+            <Typing.Delay ms = {0} />
+            
+            <span> 
+              Create a Profile and Save Music to Playlists.  
+            </span>
+            <Typing.Backspace count = {45} delay = {500} /> 
+            <Typing.Delay ms = {0} />
+
+            <span> 
+              Get Your Music Heard and More! 
+            </span>
+            
+            <Typing.Backspace count = {30} delay = {500} /> 
+            <Typing.Reset count={2} delay={900} />
+            </Typing>
+            
+            
+         
+            
+            
+             </p> 
           
           <br /> 
           
